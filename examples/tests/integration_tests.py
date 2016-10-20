7d691d7212872967c3c0ec0e423ad31703143ecc
Tim Mahrt <timmahrt@gmail.com>
2016-10-20 12:17:21 +0200

    FEATURE: Added integration tests

---
+++ examples/tests/integration_tests.py
@@ -0,0 +1,73 @@
+'''
+Created on Oct 20, 2016
+
+@author: tmahrt
+
+Runs integration tests
+
+The examples were all written as scripts.  They weren't meant to be 
+imported or run from other code.  So here, the integration test is just
+importing the scripts, which causes them to execute.  If the code completes
+with no errors, then the code is at least able to complete.
+
+Testing whether or not the code actually did what it is supposed to is
+another issue and will require some refactoring.
+'''
+
+import unittest
+import os
+import sys
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+_root = os.path.split(cwd)[0]
+sys.path.append(_root)
+
+class IntegrationTests(unittest.TestCase):
+    """Integration tests"""
+
+    def test_add_tiers(self):
+        """Running 'basic_examples.py'"""
+        print("\nbasic_examples.py" + "\n" + "-" * 10)
+        import basic_examples
+
+    def test_calculate_duration(self):
+        """Running 'dictionary_search.py'"""
+        print("\ndictionary_search.py" + "\n" + "-" * 10)
+        import dictionary_search
+    
+    def test_delete_vowels(self):
+        """Running 'syllabify_textgrid.py'"""
+        print("\nsyllabify_textgrid.py" + "\n" + "-" * 10)
+        import syllabify_textgrid
+    
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        root = os.path.join(_root, "files")
+        self.oldRoot = os.getcwd()
+        os.chdir(_root)
+        self.startingList = os.listdir(root)
+        self.startingDir = os.getcwd()
+    
+    def tearDown(self):
+        '''Remove any files generated during the test'''
+        #unittest.TestCase.tearDown(self)
+        
+        root = os.path.join(".", "files")
+        endingList = os.listdir(root)
+        endingDir = os.getcwd()
+        rmList = [fn for fn in endingList if fn not in self.startingList]
+        
+        if self.oldRoot == root:
+            for fn in rmList:
+                fnFullPath = os.path.join(root, fn)
+                if os.path.isdir(fnFullPath):
+                    os.rmdir(fnFullPath)
+                else:
+                    os.remove(fnFullPath)
+        
+        os.chdir(self.oldRoot)
+        
+
+if __name__ == '__main__':
+    unittest.main()
