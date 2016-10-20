2151b4e515af73b6116ee86ca2092f64f2520f01
Tim Mahrt <timmahrt@gmail.com>
2016-10-20 14:55:12 +0200

    DOCUMENTATION: Updates for setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ Created on Oct 15, 2014
 
 @author: tmahrt
 '''
+from setuptools import setup
 import codecs
-from distutils.core import setup
 setup(name='pysle',
       version='1.4.0',
       author='Tim Mahrt',
       author_email='timmahrt@gmail.com',
+      url='https://github.com/timmahrt/pysle',
       package_dir={'pysle':'pysle'},
       packages=['pysle'],
       license='LICENSE',
-      long_description=codecs.open('README.rst', 'r', encoding="utf-8").read(),
-#       install_requires=[], # No requirements! # requires 'from setuptools import setup'
+      long_description=codecs.open('README.rst', 'r', encoding="utf-8").read()
       )
